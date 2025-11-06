80fb3db9adbfefb8d3595d2fc639cafbdf7b4757
Bacon Bits <melvintan938@gmail.com>
2025-11-06 17:08:22 +0800

    implemented pollng and interrupt can check if ok

--- MCXC444/main.c
+++ MCXC444/main.c
@@ -1,239 +1,213 @@
-#include "MKL25Z4.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
-#include "semphr.h"
-#include "led.h"
-#include "soilMoisture.h"
-#include "lightSensor.h"
-#include "waterPump.h"
-#include "uart_comm.h"
+/*
+ * Copyright 2016-2025 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
-#define UPDATE_INTERVAL_MS 1000
-#define MIN_WATER_LEVEL 20
-#define MAX_PUMP_DURATION_MS 5000
+/**
+ * @file    timer_demo.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "fsl_debug_console.h"
+#include <math.h>
+/* TODO: insert other include files here. */
 
-#define SENSOR_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
-#define CONTROL_TASK_PRIORITY (tskIDLE_PRIORITY + 3)
-#define UART_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
-#define SAFETY_TASK_PRIORITY (tskIDLE_PRIORITY + 4)
+/* TODO: insert other definitions and declarations here. */
 
-#define SENSOR_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE * 2)
-#define CONTROL_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE * 2)
-#define UART_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE * 3)
-#define SAFETY_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE)
+// LED pin numbers
+#define REDLED		4 //PTA4
+#define GREENLED	4 //PTD4
+#define BLUELED		5 //PTA5
+#define LDRPIN      2 //PTC2
+#define SOILPIN     1 //PTA1
+#define PUMPPIN		2 //PTA2
 
-typedef enum {
-    MODE_AUTO = 0,
-    MODE_MANUAL = 1
-} SystemMode_t;
+volatile bool relayState = false;  // Track current relay state (false = off, true = on)
+volatile int lastSoilState = 0;    // Track last soil moisture state
 
-typedef struct {
-    uint8_t soilMoisture;
-    uint8_t lightLevel;
-    uint8_t waterLevel;
-} SensorData_t;
 
-static SystemMode_t systemMode = MODE_AUTO;
-static uint8_t waterLevel = 100;
-static TickType_t pumpStartTicks = 0;
+void initLEDs() {
+	SIM->SCGC5 |= (SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTD_MASK);
+	PORTA->PCR[REDLED] &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[REDLED] |= PORT_PCR_MUX(1);
 
-static QueueHandle_t sensorDataQueue;
-static QueueHandle_t commandQueue;
-static SemaphoreHandle_t systemModeMutex;
-static SemaphoreHandle_t pumpControlMutex;
+	PORTA->PCR[BLUELED] &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[BLUELED] |= PORT_PCR_MUX(1);
 
-void SensorTask(void *pvParameters);
-void ControlTask(void *pvParameters);
-void UARTTask(void *pvParameters);
-void SafetyTask(void *pvParameters);
+	PORTD->PCR[GREENLED] &= PORT_PCR_MUX_MASK;
+	PORTD->PCR[GREENLED] |= PORT_PCR_MUX(1);
+
+	// Set PDDR
+	GPIOA->PDDR |= ((1 << REDLED) | (1 << BLUELED));
+	GPIOD->PDDR |= (1 << GREENLED);
+}
+
+// Switch off all LEDs
+void ledOff() {
+	GPIOA->PCOR |= ((1 << REDLED) | (1 << BLUELED));
+	GPIOD->PCOR |= (1 << GREENLED);
+}
+
+// Switch on all LEDs
+void ledOn() {
+	GPIOA->PSOR |= ((1 << REDLED) | (1 << BLUELED));
+	GPIOD->PSOR |= (1 << GREENLED);
+}
+
+// Initialize interrupt for LDR (Light Dependent Resistor)
+void initLDR() {
+    SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+
+    // Configure as GPIO
+    PORTC->PCR[LDRPIN] &= ~PORT_PCR_MUX_MASK;
+    PORTC->PCR[LDRPIN] |= PORT_PCR_MUX(1);
+
+    // Enable pull-up resistor
+    PORTC->PCR[LDRPIN] |= PORT_PCR_PE_MASK;
+    PORTC->PCR[LDRPIN] |= PORT_PCR_PS_MASK;
+
+    // Set as input
+    GPIOC->PDDR &= ~(1 << LDRPIN);
+}
+
+
+
+void initPump(){
+	SIM->SCGC5 |= (SIM_SCGC5_PORTA_MASK);
+	PORTA->PCR[PUMPPIN] &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[PUMPPIN] |= PORT_PCR_MUX(1);
+	GPIOA->PDDR |= (1 << PUMPPIN);
+}
+
+void pumpOff(){
+	GPIOA->PCOR |= (1 << PUMPPIN);
+}
+
+void pumpOn(){
+	GPIOA->PSOR |= (1 << PUMPPIN);
+}
+
+void initSOILInterrupt() {
+    // Disable PORTE_D Interrupts
+    NVIC_DisableIRQ(PORTA_IRQn);
+
+    // LDRPIN is at PTC3. Enable port C
+    SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;
+
+    // Configure as GPIO
+    PORTA->PCR[SOILPIN] &= ~PORT_PCR_MUX_MASK;
+    PORTA->PCR[SOILPIN] |= PORT_PCR_MUX(1);
+
+    // Configure pullup
+    PORTA->PCR[SOILPIN] &= ~PORT_PCR_PE_MASK;
+    PORTA->PCR[SOILPIN] |= PORT_PCR_PE(1);
+    PORTA->PCR[SOILPIN] &= ~PORT_PCR_PS_MASK;
+    PORTA->PCR[SOILPIN] |= PORT_PCR_PS(1);
+
+    // Configure as input
+    GPIOA->PDDR &= ~(1 << SOILPIN);
+
+    PORTA->PCR[SOILPIN] &= ~PORT_PCR_PE_MASK;
+    PORTA->PCR[SOILPIN] |= PORT_PCR_PE(1);
+
+    PORTA->PCR[SOILPIN] &= ~PORT_PCR_IRQC_MASK;
+    PORTA->PCR[SOILPIN] |= PORT_PCR_IRQC(0b1011);
+
+    // Set lowest priority
+    NVIC_SetPriority(PORTA_IRQn, 192);
+
+    // Clear pending interrupts
+    NVIC_ClearPendingIRQ(PORTA_IRQn);
+
+    // Enable interrupts
+    NVIC_EnableIRQ(PORTA_IRQn);
+}
+
+
+// Polling for LDR (light level)
+void pollLDR() {
+    // Read the LDR sensor state (GPIO input read)
+    int ldrState = GPIOC->PDIR & (1 << LDRPIN);  // Read pin value for LDR
+
+    // If the LDR state is not 0 (not dark), turn LEDs on
+    if (ldrState != 0) {
+        ledOn();  // Turn the LEDs on
+    } else {
+        ledOff();  // Turn the LEDs off when it's dark
+    }
+}
+
+void PORTA_IRQHandler() {
+	NVIC_ClearPendingIRQ(PORTA_IRQn);
+
+	if (PORTA->ISFR & (1 << SOILPIN)) {
+		// Read the current soil moisture level
+		int currentSoilState = GPIOA->PDIR & (1 << SOILPIN);
+
+		// Check if the soil state has changed (from dry to wet or vice versa)
+		if (currentSoilState != lastSoilState) {
+			// Toggle the relay if the state changes
+			relayState = !relayState;
+
+			// Toggle pump based on relay state
+			if (relayState) {
+				pumpOn();  // Turn on the water pump
+				PRINTF("Water pump ON\r\n");
+			} else {
+				pumpOff(); // Turn off the water pump
+				PRINTF("Water pump OFF\r\n");
+			}
+
+			// Update the last soil state
+			lastSoilState = currentSoilState;
+		}
+	}
+
+	PORTA->ISFR |= (1 << SOILPIN);
+}
+
 
 int main(void) {
-    LED_Init();
-    SoilMoisture_Init();
-    LightSensor_Init();
-    WaterPump_Init();
-    UART_Init();
-    
-    LED_On(LED_RED);
-    vTaskDelay(pdMS_TO_TICKS(200));
-    LED_Off(LED_RED);
-    LED_On(LED_GREEN);
-    vTaskDelay(pdMS_TO_TICKS(200));
-    LED_Off(LED_GREEN);
-    LED_On(LED_BLUE);
-    vTaskDelay(pdMS_TO_TICKS(200));
-    LED_AllOff();
-    
-    sensorDataQueue = xQueueCreate(5, sizeof(SensorData_t));
-    commandQueue = xQueueCreate(10, sizeof(Command_t));
-    systemModeMutex = xSemaphoreCreateMutex();
-    pumpControlMutex = xSemaphoreCreateMutex();
-    
-    if (sensorDataQueue == NULL || commandQueue == NULL || 
-        systemModeMutex == NULL || pumpControlMutex == NULL) {
-        UART_SendString("ERROR: Failed to create FreeRTOS objects\n");
-        while(1);
-    }
-    
-    xTaskCreate(SensorTask, "Sensor", SENSOR_TASK_STACK_SIZE, NULL, SENSOR_TASK_PRIORITY, NULL);
-    xTaskCreate(ControlTask, "Control", CONTROL_TASK_STACK_SIZE, NULL, CONTROL_TASK_PRIORITY, NULL);
-    xTaskCreate(UARTTask, "UART", UART_TASK_STACK_SIZE, NULL, UART_TASK_PRIORITY, NULL);
-    xTaskCreate(SafetyTask, "Safety", SAFETY_TASK_STACK_SIZE, NULL, SAFETY_TASK_PRIORITY, NULL);
-    
-    UART_SendString("FreeRTOS Plant Watering System Started\n");
-    
-    vTaskStartScheduler();
-    
-    while(1);
-}
 
-void SensorTask(void *pvParameters) {
-    SensorData_t sensorData;
-    TickType_t xLastWakeTime = xTaskGetTickCount();
-    const TickType_t xFrequency = pdMS_TO_TICKS(100);
-    
+    /* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+    /* Init FSL debug console. */
+    BOARD_InitDebugConsole();
+#endif
+
+    // Initialize LEDs
+    initLEDs();
+
+    // Switch off LEDs
+    ledOff();
+
+    initPump();
+
+    pumpOff();
+
+    initLDR();
+
+    // Initialize SW2 and SW3 interrupt
+
+    initSOILInterrupt();
+
+    /* Force the counter to be placed into memory. */
+    /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
-        sensorData.soilMoisture = SoilMoisture_Read();
-        sensorData.lightLevel = LightSensor_Read();
-        sensorData.waterLevel = waterLevel;
-        
-        if (xQueueSend(sensorDataQueue, &sensorData, 0) != pdPASS) {
-            UART_SendString("WARNING: Sensor queue full\n");
-        }
-        
-        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+        // Poll the LDR (light sensor) to control the LEDs
+        pollLDR();
+
+        // Optionally, add a small delay here for polling rate control
+        // (not needed in this case, unless you want to slow down polling)
     }
-}
-
-void ControlTask(void *pvParameters) {
-    SensorData_t sensorData;
-    SystemMode_t currentMode;
-    
-    while(1) {
-        if (xQueueReceive(sensorDataQueue, &sensorData, portMAX_DELAY) == pdPASS) {
-            if (xSemaphoreTake(systemModeMutex, pdMS_TO_TICKS(100)) == pdTRUE) {
-                currentMode = systemMode;
-                xSemaphoreGive(systemModeMutex);
-                
-                if (currentMode == MODE_AUTO) {
-                    if (LightSensor_IsDark()) {
-                        LED_On(LED_BLUE);
-                    } else {
-                        LED_Off(LED_BLUE);
-                    }
-                    
-                    if (xSemaphoreTake(pumpControlMutex, pdMS_TO_TICKS(100)) == pdTRUE) {
-                        if (SoilMoisture_IsDry() && sensorData.waterLevel > MIN_WATER_LEVEL) {
-                            if (!WaterPump_GetStatus()) {
-                                WaterPump_On();
-                                pumpStartTicks = xTaskGetTickCount();
-                                LED_On(LED_GREEN);
-                            }
-                        } else {
-                            WaterPump_Off();
-                            LED_Off(LED_GREEN);
-                        }
-                        xSemaphoreGive(pumpControlMutex);
-                    }
-                }
-            }
-        }
-    }
-}
-
-void UARTTask(void *pvParameters) {
-    SystemStatus_t status;
-    Command_t cmd;
-    uint8_t cmdData;
-    TickType_t xLastStatusTime = xTaskGetTickCount();
-    const TickType_t xStatusInterval = pdMS_TO_TICKS(UPDATE_INTERVAL_MS);
-    
-    while(1) {
-        cmd = UART_ReceiveCommand(&cmdData);
-        
-        if (cmd != CMD_NONE) {
-            if (xSemaphoreTake(systemModeMutex, pdMS_TO_TICKS(100)) == pdTRUE) {
-                switch (cmd) {
-                    case CMD_PUMP_ON:
-                        systemMode = MODE_MANUAL;
-                        if (xSemaphoreTake(pumpControlMutex, pdMS_TO_TICKS(100)) == pdTRUE) {
-                            WaterPump_On();
-                            pumpStartTicks = xTaskGetTickCount();
-                            xSemaphoreGive(pumpControlMutex);
-                        }
-                        UART_SendString("Pump ON (Manual)\n");
-                        break;
-                        
-                    case CMD_PUMP_OFF:
-                        if (xSemaphoreTake(pumpControlMutex, pdMS_TO_TICKS(100)) == pdTRUE) {
-                            WaterPump_Off();
-                            xSemaphoreGive(pumpControlMutex);
-                        }
-                        UART_SendString("Pump OFF (Manual)\n");
-                        break;
-                        
-                    case CMD_LED_ON:
-                        systemMode = MODE_MANUAL;
-                        LED_On(LED_BLUE);
-                        UART_SendString("LED ON (Manual)\n");
-                        break;
-                        
-                    case CMD_LED_OFF:
-                        LED_Off(LED_BLUE);
-                        UART_SendString("LED OFF (Manual)\n");
-                        break;
-                        
-                    default:
-                        break;
-                }
-                xSemaphoreGive(systemModeMutex);
-            }
-        }
-        
-        if ((xTaskGetTickCount() - xLastStatusTime) >= xStatusInterval) {
-            status.soilMoisture = SoilMoisture_Read();
-            status.lightLevel = LightSensor_Read();
-            status.waterLevel = waterLevel;
-            status.pumpStatus = WaterPump_GetStatus();
-            status.ledStatus = LED_GetStatus(LED_BLUE);
-            
-            UART_SendStatus(&status);
-            xLastStatusTime = xTaskGetTickCount();
-        }
-        
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
-}
-
-void SafetyTask(void *pvParameters) {
-    TickType_t currentTicks;
-    
-    while(1) {
-        if (xSemaphoreTake(pumpControlMutex, pdMS_TO_TICKS(100)) == pdTRUE) {
-            if (WaterPump_GetStatus()) {
-                currentTicks = xTaskGetTickCount();
-                
-                if ((currentTicks - pumpStartTicks) > pdMS_TO_TICKS(MAX_PUMP_DURATION_MS)) {
-                    WaterPump_Off();
-                    UART_SendString("SAFETY: Pump auto-off (timeout)\n");
-                }
-            }
-            xSemaphoreGive(pumpControlMutex);
-        }
-        
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-}
-
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
-    UART_SendString("ERROR: Stack overflow in task: ");
-    UART_SendString(pcTaskName);
-    UART_SendString("\n");
-    while(1);
-}
-
-void vApplicationMallocFailedHook(void) {
-    UART_SendString("ERROR: Malloc failed\n");
-    while(1);
+    return 0 ;
 }
